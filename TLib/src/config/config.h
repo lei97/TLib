4f0a1758fc1fae5f54bfe96871daecd160c1e68b
lei <1022958177@qq.com>
2019-12-08 17:55:59 +0800

    添加项目文件。

---
+++ TLib/src/config/config.h
@@ -0,0 +1,55 @@
+/*
+@tlei 
+data：2019/12/08
+function: creat Config of class
+    1. use json data 
+*/
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#include <string>
+#include <map>
+#include <fstream>
+#include <iostream>
+#ifdef _WIN32
+#endif
+#include <json.h>
+
+using json = nlohmann::json;
+
+namespace tlib {
+    using std::string;
+    using std::map;
+    class Config {
+    public:
+        Config();
+        json get();
+    private:
+        void readFile();
+        json m_data;
+        string m_fileName;
+    };
+}
+
+namespace tlib {
+    Config::Config()
+    {
+
+    } 
+
+    json Config::get()
+    {
+        return m_data;
+    }
+
+    void Config::readFile()
+    {
+        std::ifstream ifs;
+        ifs.open(m_fileName);// 读取配置文件
+        if (ifs.is_open() == NULL)
+        {
+            throw;      //TODO: 异常处理类待整理，暂定
+        }
+    }
+}
+#endif
---
+++ TLib/src/main.cpp
@@ -0,0 +1,10 @@
+#include "json.h"
+
+int main()
+{
+    while (true)
+    {
+  
+    }
+	return 0;
+}
